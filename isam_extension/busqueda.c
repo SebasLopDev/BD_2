afcde69eb9138f2a0e73d601c6747d13a6d47e14
Meristyn <117225095+Meristyn@users.noreply.github.com>
2026-09-15 18:00:57 -0500

    Implementación de la función buscar_página con búsqueda binaria

--- isam_extension/busqueda.c
+++ isam_extension/busqueda.c
@@ -1,16 +1,36 @@
 // busqueda.c
 #include "isam_core.h"
+#include <stddef.h> 
 
-// Si necesitan exponer esto a SQL más adelante, usarán macros de PG. 
-// Por ahora, lógica pura en C.
+// Este se conectará al array global
+
+extern IndexEntry* indice_estatico;
+extern int total_entradas_indice;
 
 DataPage* buscar_pagina(int clave) {
-    DataPage* pagina_destino = NULL;
-    
-    // TODO (María): 
-    // 1. Cargar o leer el archivo del índice estático.
-    // 2. Hacer búsqueda binaria o navegación en el índice.
-    // 3. Retornar el puntero a la DataPage donde debería estar la clave.
-    
-    return pagina_destino;
+    if (total_entradas_indice == 0 || indice_estatico == NULL) {
+        return NULL; 
+    }
+
+    int izquierda = 0;
+    int derecha = total_entradas_indice - 1;
+    DataPage* pagina_destino = NULL; 
+
+    while (izquierda <= derecha) {
+        int medio = izquierda + (derecha - izquierda) / 2;
+
+        if (indice_estatico[medio].first_key == clave) {
+            return indice_estatico[medio].page_ptr;
+            
+        } else if (indice_estatico[medio].first_key < clave) {
+            // Se guarda como candidata de límite inferior para búsqueda por rango
+            pagina_destino = indice_estatico[medio].page_ptr;
+            izquierda = medio + 1; 
+            
+        } else {
+            derecha = medio - 1;
+        }
+    }
+
+    return pagina_destino; 
 }
